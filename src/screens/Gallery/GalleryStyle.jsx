4d77fee53116a3eb88653b59d896d89ed8ae808f
Amir <106912785+am1r0d@users.noreply.github.com>
2023-09-24 01:47:52 +0330

    Building gallery screen

--- src/screens/Gallery/GalleryStyle.jsx
+++ src/screens/Gallery/GalleryStyle.jsx
@@ -1,24 +1,19 @@
 import { StyleSheet } from "react-native";
 import { colors } from "../../constants/color";
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        position: "relative",
+        // paddingHorizontal: 32,
     },
-    subtitle: {
-        fontSize: 20,
-        color: colors.black,
-        marginTop: 40,
-        // marginBottom: 18,
+    image: {
+        width: "100%",
+        height: 400,
+        borderRadius: 20,
+        marginTop: 24,
     },
-    row: {
-        flexDirection: "row",
-        flexWrap: "wrap",
-    },
-    emptyText: {
-        textAlign: "center",
-        marginTop: 20,
-        fontSize: 15,
-        color: "rgba(0,0,0,0.5)",
-    },
+    backContainer: { position: "absolute", margin: 40 },
+    backIcon: { width: 40, height: 40 },
 });
 export default styles;
