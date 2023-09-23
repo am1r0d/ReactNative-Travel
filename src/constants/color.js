d1dbb8751bb44b312cf270a9e3852add2dc5f4e2
Amir <106912785+am1r0d@users.noreply.github.com>
2023-09-24 01:09:19 +0330

    Create mini image gallery

--- src/constants/color.js
+++ src/constants/color.js
@@ -2,4 +2,5 @@ export const colors = {
     blue1: "#4681A3",
     black: "#000000",
     grey: "#E2E2E2",
+    white: "#FFFFFF",
 };
--- src/screens/AttractionDetails/AttractionDetails.jsx
+++ src/screens/AttractionDetails/AttractionDetails.jsx
@@ -13,10 +13,20 @@ import styles from "./AttractionDetailsStyle";
 const AttractionDetails = ({ route, navigation }) => {
     const { item } = route?.params || {};
     const mainImage = item?.images?.length ? item?.images[0] : null;
+
     // back e.
     const onBack = () => {
         navigation.goBack();
     };
+
+    //
+    const onGalleryNavigate = () => {
+        navigation.navigate("Gallery", { images: item?.images });
+    };
+
+    const slicedImages = item?.images?.length ? item?.images?.slice(0, 5) : [];
+    const diffImages = item?.images?.length - slicedImages?.length;
+
     //
     return (
         <SafeAreaView style={styles.container}>
@@ -39,17 +49,26 @@ const AttractionDetails = ({ route, navigation }) => {
                         />
                     </Pressable>
                 </View>
-                <View style={styles.footer}>
-                    {item?.images?.length
-                        ? item?.images?.map((image) => (
-                              <Image
-                                  key={image}
-                                  source={{ uri: image }}
-                                  style={styles.miniImage}
-                              />
-                          ))
-                        : null}
-                </View>
+
+                {/*  */}
+                <Pressable onPress={onGalleryNavigate} style={styles.footer}>
+                    {slicedImages?.map((image, index) => (
+                        <View key={image}>
+                            <Image
+                                source={{ uri: image }}
+                                style={styles.miniImage}
+                            />
+                            {diffImages > 0 &&
+                            index === slicedImages?.length - 1 ? (
+                                <View style={styles.moreImagesContainer}>
+                                    <Text
+                                        style={styles.moreImages}
+                                    >{`+${diffImages}`}</Text>
+                                </View>
+                            ) : null}
+                        </View>
+                    ))}
+                </Pressable>
             </ImageBackground>
             <Text>{item?.name}</Text>
         </SafeAreaView>
